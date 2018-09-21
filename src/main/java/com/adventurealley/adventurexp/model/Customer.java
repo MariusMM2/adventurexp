b8552ee92e600ae880ecbda4b1f610137baddeaf
Cons19 <razvan1598@gmail.com>
2018-09-21 12:44:23 +0200

    Customer model done

--- src/main/java/com/adventurealley/adventurexp/model/Customer.java
+++ src/main/java/com/adventurealley/adventurexp/model/Customer.java
@@ -1,4 +1,57 @@
 package com.adventurealley.adventurexp.model;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Customer {
+    private int id;
+    private String name;
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private LocalDate timeSlot;
+    private int age;
+
+    public Customer() {
+
+    }
+
+    public Customer(int id, String name, LocalDate timeSlot, int age) {
+        this.id = id;
+        this.name = name;
+        this.timeSlot = timeSlot;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getTimeSlot() {
+        return timeSlot;
+    }
+
+    public void setTimeSlot(LocalDate timeSlot) {
+        this.timeSlot = timeSlot;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
