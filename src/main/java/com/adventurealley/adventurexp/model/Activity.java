ff9041626210fff65a093eec461b82b0b67dc033
albertturcu <turcu.albert98@gmail.com>
2018-09-25 14:01:34 +0200

    dwada

--- src/main/java/com/adventurealley/adventurexp/model/Activity.java
+++ src/main/java/com/adventurealley/adventurexp/model/Activity.java
@@ -1,4 +1,101 @@
 package com.adventurealley.adventurexp.model;
 
+import javax.swing.*;
+
 public class Activity {
+
+    private String title;
+    private int capacity;
+    private String period;
+    private int price;
+    private String location;
+    private int ageLimit;
+    private String Description;
+    private String staff;
+
+    public int getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(int activityId) {
+        this.activityId = activityId;
+    }
+
+    private int activityId;
+
+    public Activity(String title, int capacity, String period, int price, String location, int ageLimit, String description, String staff, int activityId) {
+        this.title = title;
+        this.capacity = capacity;
+        this.period = period;
+        this.price = price;
+        this.location = location;
+        this.ageLimit = ageLimit;
+        this.Description = description;
+        this.staff = staff;
+        this.activityId = activityId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(String period) {
+        this.period = period;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getAgeLimit() {
+        return ageLimit;
+    }
+
+    public void setAgeLimit(int ageLimit) {
+        this.ageLimit = ageLimit;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getStaff() {
+        return staff;
+    }
+
+    public void setStaff(String staff) {
+        this.staff = staff;
+    }
 }
--- src/main/java/com/adventurealley/adventurexp/repositories/ActivityRepository.java
+++ src/main/java/com/adventurealley/adventurexp/repositories/ActivityRepository.java
@@ -2,9 +2,19 @@ package com.adventurealley.adventurexp.repositories;
 
 import com.adventurealley.adventurexp.model.Activity;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ActivityRepository implements IRepository<Activity> {
+
+    private Connection conn;
+    private PreparedStatement preparedStatement;
+    private ResultSet resultSet;
+
+
     @Override
     public boolean create(Activity item) {
         return false;
@@ -12,7 +22,29 @@ public class ActivityRepository implements IRepository<Activity> {
 
     @Override
     public Activity read(int itemId) {
-        return null;
+        try {
+            preparedStatement = conn.prepareStatement("select * from activity");
+            preparedStatement.setInt(1, itemId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+
+            return new Activity(
+                    resultSet.getString("title"),
+                    resultSet.getInt("capacity"),
+                    resultSet.getString("period"),
+                    resultSet.getInt("price"),
+                    resultSet.getString("location"),
+                    resultSet.getInt("ageLimit"),
+                    resultSet.getString("description"),
+                    resultSet.getString("staff"),
+                    resultSet.getInt("activtyId")
+            );
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();;
+            return null;
+        }
     }
 
     @Override
