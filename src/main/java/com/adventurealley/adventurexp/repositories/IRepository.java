e1ba89d23e6b6fe51a0e14ce20e325f1d71f8d14
MariusMM2 <dmunteanu91@yahoo.com>
2018-10-02 13:49:36 +0200

    added TimerTask to refresh the db connection

--- src/main/java/com/adventurealley/adventurexp/repositories/IRepository.java
+++ src/main/java/com/adventurealley/adventurexp/repositories/IRepository.java
@@ -5,13 +5,36 @@ import com.adventurealley.adventurexp.repositories.util.DBConnection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public abstract class IRepository<T> {
 
-    Connection connection = DBConnection.getConnection();
+    static Connection connection;
     PreparedStatement preparedStatement;
     ResultSet resultSet;
+
+    static {
+        //creates a Runnable object to hold the instructions for the refreshing thread
+        TimerTask refreshTask = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("refreshing");
+                //closes the current connection
+                try {
+                    connection.close();
+                } catch(SQLException e) {
+                    e.printStackTrace();
+                }
+                //reconnects to the database
+                connection = DBConnection.getConnection();
+            }
+        };
+        connection = DBConnection.getConnection();
+        new Timer().scheduleAtFixedRate(refreshTask, 0, 1000 * 60 * 2);
+    }
 
     abstract public int create(T item);
     abstract public T read(int itemId);
